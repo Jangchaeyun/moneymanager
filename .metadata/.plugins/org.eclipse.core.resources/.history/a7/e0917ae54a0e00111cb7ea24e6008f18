edd2d8b67fe52ee58715d7bf09f1811d1b620dcd
Jangchaeyun <zzangsally@naver.com>
2026-02-20 22:50:33 +0900

    0220 ProfileDTO

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/a7/e0917ae54a0e00111cb7ea24e6008f18
@@ -0,0 +1,27 @@
+package com.cherry.manager.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tbl_profiles")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProfileEntity {
+	private Long id;
+	private String fullName;
+	private String email;
+	private String password;
+	private String profileImageUrl;
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
+	private Boolean isActive;
+}
--- moneymanager/src/main/java/com/cherry/manager/entity/ProfileEntity.java
+++ moneymanager/src/main/java/com/cherry/manager/entity/ProfileEntity.java
@@ -2,7 +2,15 @@ package com.cherry.manager.entity;
 
 import java.time.LocalDateTime;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,12 +24,27 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class ProfileEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String fullName;
+	@Column(unique = true)
 	private String email;
 	private String password;
 	private String profileImageUrl;
+	@Column(updatable = false)
+	@CreationTimestamp
 	private LocalDateTime createdAt;
+	@UpdateTimestamp
 	private LocalDateTime updatedAt;
 	private Boolean isActive;
+	private String activationToken;
+	
+	@PrePersist
+	public void prePersist() {
+		if (this.isActive == null) {
+			isActive = false;
+		}
+	}
 }
