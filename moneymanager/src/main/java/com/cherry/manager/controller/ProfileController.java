f32e65084770d13fa9bbd94c9f294727ff7cbd5f
Jangchaeyun <zzangsally@naver.com>
2026-02-21 23:03:45 +0900

    0221 Profile 컨트롤러, 서비스

---
+++ moneymanager/src/main/java/com/cherry/manager/controller/ProfileController.java
@@ -0,0 +1,24 @@
+package com.cherry.manager.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cherry.manager.dto.ProfileDTO;
+import com.cherry.manager.service.ProfileService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class ProfileController {
+	private final ProfileService profileService;
+	
+	@PostMapping("/register")
+	public ResponseEntity<ProfileDTO> registerProfile(@RequestBody ProfileDTO profileDTO) {
+		ProfileDTO registeredProfile = profileService.registerProfile(profileDTO);
+		return ResponseEntity.status(HttpStatus.CREATED).body(registeredProfile);
+	}
+}
--- moneymanager/src/main/java/com/cherry/manager/service/ProfileService.java
+++ moneymanager/src/main/java/com/cherry/manager/service/ProfileService.java
@@ -1,5 +1,7 @@
 package com.cherry.manager.service;
 
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 
 import com.cherry.manager.dto.ProfileDTO;
@@ -14,10 +16,34 @@ public class ProfileService {
 	private final ProfileRepository profileRepository;
 	
 	public ProfileDTO registerProfile(ProfileDTO profileDTO) {
-		return profileDTO;
+		ProfileEntity newProfile = toEntity(profileDTO);
+		
+		newProfile.setActivationToken(UUID.randomUUID().toString());
+		newProfile = profileRepository.save(newProfile);
+		
+		return toDTO(newProfile);
 	}
 	
 	public ProfileEntity toEntity(ProfileDTO profileDTO) {
-
+		return ProfileEntity.builder()
+				.id(profileDTO.getId())
+				.fullName(profileDTO.getFullName())
+				.email(profileDTO.getEmail())
+				.password(profileDTO.getPassword())
+				.profileImageUrl(profileDTO.getProfileImageUrl())
+				.createdAt(profileDTO.getCreatedAt())
+				.updatedAt(profileDTO.getUpdatedAt())
+				.build();
+	}
+	
+	public ProfileDTO toDTO(ProfileEntity profileEntity) {
+		return ProfileDTO.builder()
+				.id(profileEntity.getId())
+				.fullName(profileEntity.getFullName())
+				.email(profileEntity.getEmail())
+				.profileImageUrl(profileEntity.getProfileImageUrl())
+				.createdAt(profileEntity.getCreatedAt())
+				.updatedAt(profileEntity.getUpdatedAt())
+				.build();
 	}
 }
